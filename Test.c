0874021e32e9b9f34e81d41c7f1a506c6c72f40a
preboy1987 <preboy1987@gmail.com>
2019-03-14 14:09:51 +0700

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,10 +1,13 @@
 include stdio.h
+include math.h
 
 void main() {
   printf("Nguyen Nam Giang");
   int a = 1;
   int b = 3;
   int count = a + b;
+  int multiple = a * b;
   printf("Tong cong la: %x", count);
+  printf("Phep nhan la: %x", multiple);
   endl();
 }
