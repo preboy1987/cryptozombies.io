226ebee062d85cb735e20f06ac4e17c826d25b79
preboy1987 <preboy1987@gmail.com>
2019-03-14 14:47:38 +0700

    Update Test.c

--- Test.c
+++ Test.c
@@ -2,11 +2,11 @@ include stdio.h
 include math.h
 
 void main() {
-  printf("Testing only");
+  printf("Testing only!!!");
   int a = 1;
   int b = 3;
-  int count = (a + b) - (a - b);
-  int multiple = (a * b) / (a * b);
+  int count = (a + b);
+  int multiple = (a * b) + count;
   printf("Tong cong la: %x", count);
   printf("Phep nhan la: %x", multiple);
   endl();
