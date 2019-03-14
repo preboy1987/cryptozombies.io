0531ea338a353214bcaadd1d3faf9276496b96dd
preboy1987 <preboy1987@gmail.com>
2019-03-14 14:45:53 +0700

    Update Test.c

--- Test.c
+++ Test.c
@@ -5,7 +5,7 @@ void main() {
   printf("Nguyen Nam Giang");
   int a = 1;
   int b = 3;
-  int count = a + b;
+  int count = (a + b) - (a - b);
   int multiple = (a * b) / (a * b);
   printf("Tong cong la: %x", count);
   printf("Phep nhan la: %x", multiple);
