933224b822decc37e128f01986b53282ee7d806b
preboy1987 <preboy1987@gmail.com>
2019-03-14 14:49:05 +0700

    Update Test.c

--- Test.c
+++ Test.c
@@ -8,7 +8,9 @@ void main() {
   int count = (a + b);
   int multiple = (a * b);
   int divide = a / b;
+  int minus = a - b;
   printf("Phep cong la: %x", count);
+  printf("Phep tru la: %x", minus);
   printf("Phep nhan la: %x", multiple);
   printf("Phep chia la: %x", divide);
   endl();
